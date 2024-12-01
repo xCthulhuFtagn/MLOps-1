50a80a86bde3b9e526a50eba1583725fad2a73d0
xftagn <mkolodyazhny@gmail.com>
2024-12-01 22:33:10 +0300

    it worked, but lib was broken

--- bash_scripts/track_datasets.sh
+++ bash_scripts/track_datasets.sh
@@ -10,19 +10,12 @@ fi
 remote=$1
 dataset_path=$2
 
-# # Ensure the dataset path is within the DVC project
-# if [[ ! "$dataset_path" =~ ^/home/owner/Documents/DEV/MLOps/HW1/ ]]; then
-#     echo "Dataset path must be within the DVC project directory."
-#     exit 1
-# fi
-
 # Add the dataset to DVC
 dvc add "$dataset_path"
-git add "'$dataset_path'.dvc"
-echo "$dataset_path"
+git add "$dataset_path.dvc"
 # Push the dataset to the specified remote
 git commit -m "Data update for '$bucket'"
-# git push
+git push
 dvc push -r "$remote"
 
-echo "Dataset '$dataset_path' tracked and pushed to remote '$remote' successfully."
+echo "Dataset '$dataset_path' tracked and pushed to remote '$bucket' successfully."
